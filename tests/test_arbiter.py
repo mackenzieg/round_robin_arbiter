a147b7e6a65dc35f9aef43878ef649c0789db5c4
mackenzieg <mackenziejgoodwin@gmail.com>
2025-08-19 18:18:37 -0700

    Started writing scoreboard

--- tests/test_arbiter.py
+++ tests/test_arbiter.py
@@ -7,6 +7,31 @@ from collections import deque
 import logging
 import random
 
+class ArbiterScoreboard:
+    def __init__(self, dut):
+        self.dut = dut
+        self.expected_grants = [queue.Queue() for _ in range(4)]
+
+        for i in range(random.randomint(15)):
+            self.expected_grants[random.randomint(0, 3)].append(1)
+
+        cocotb.start_soon(self.req_inserter(dut))
+        
+    async def req_inserter(self, dut):
+        while (True):
+            have_entries = False
+            for i in range(4):
+                if (not self.expected_grants[i].empty()):
+                    have_entries = True
+
+            if (have_entries):
+                value = 0
+                for i in range(4):
+                    if (not self.expected_grants[i].empty()):
+                        value |= (1 << i)
+
+            await RisingEdge(dut.clk)
+
 @cocotb.test()
 async def arbiter_basic_test(dut):
     log = logger = logging.getLogger(__name__)
