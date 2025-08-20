21a230f006e3a6ad830ca5cd757b90a4cab115be
mackenzieg <mackenziejgoodwin@gmail.com>
2025-08-19 18:30:44 -0700

    Debugging a freeze

--- tests/test_arbiter.py
+++ tests/test_arbiter.py
@@ -11,11 +11,13 @@ class ArbiterScoreboard:
     def __init__(self, dut):
         self.dut = dut
         self.expected_grants = [queue.Queue() for _ in range(4)]
+        self.log = logging.getLogger(__name__)
 
-        for i in range(random.randomint(15)):
-            self.expected_grants[random.randomint(0, 3)].append(1)
+        for i in range(random.randint(6, 10)):
+            self.expected_grants[random.randint(0, 3)].put(1)
 
         cocotb.start_soon(self.req_inserter(dut))
+        cocotb.start_soon(self.grant_polling(dut))
         
     async def req_inserter(self, dut):
         while (True):
@@ -24,19 +26,34 @@ class ArbiterScoreboard:
                 if (not self.expected_grants[i].empty()):
                     have_entries = True
 
+            value = 0
             if (have_entries):
-                value = 0
                 for i in range(4):
                     if (not self.expected_grants[i].empty()):
                         value |= (1 << i)
 
+            self.dut.req.value = value
+
             await RisingEdge(dut.clk)
+
+    async def grant_polling(self, dut):
+        while (True):
+            value = self.dut.grant.value
+
+            for i in range(4):
+                if ((value >> i) & 1):
+                    self.expected_grants[i].get()
+
+            await RisingEdge(dut.clk)
+
 
 @cocotb.test()
 async def arbiter_basic_test(dut):
     log = logger = logging.getLogger(__name__)
 
     inserted = [queue.Queue() for _ in range(4)]
+
+    arbiter_scoreboard = ArbiterScoreboard(dut)
 
     # Start clock
     cocotb.start_soon(Clock(dut.clk, 20, units="ns").start())
